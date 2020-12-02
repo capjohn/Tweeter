23be9920fc98a63b7be2c8857d9331b103ac713c
capjohn <74626164+capjohn@users.noreply.github.com>
2020-12-03 00:48:02 +0100

    Update User.php

--- src/tweeterapp/model/User.php
+++ src/tweeterapp/model/User.php
@@ -10,15 +10,15 @@ class User extends \Illuminate\Database\Eloquent\Model {
     }
     //Récupère les followers de l'utilisateur(followee) dans la table Follow
     public function followedBy(){
-        return $this->belongsToMany('tweeterapp\model\User','Follow','followee','follower');
+        return $this->belongsToMany('tweeterapp\model\User','follow','followee','follower');
     }
     //Récupère l'id du tweet et l'id du user ayant like le tweet. Dans la table Like
     public function liked(){
-        return $this->belongsToMany('tweeterapp\model\Tweet','Like','user_id','tweet_id');
+        return $this->belongsToMany('tweeterapp\model\Tweet','like','user_id','tweet_id');
     }
     //Récupère les id des followers que l'utilisateur suit dans la table Follow
     public function follows(){
-        return $this->belongsToMany('tweeterapp\model\User','Follow','follower','followee');
+        return $this->belongsToMany('tweeterapp\model\User','follow','follower','followee');
     }
     //Récupère comme FollowedBy les followers de l'utilisateur et compte son nombre
     public function followedCount(){
